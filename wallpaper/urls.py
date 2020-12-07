9007b7b74e96f7629eac86f61ddea01acceb3230
Vijender341 <74849495+Vijender341@users.noreply.github.com>
2020-12-07 06:26:38 +0530

    Update urls.py

--- wallpaper/urls.py
+++ wallpaper/urls.py
@@ -22,5 +22,6 @@ from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('aboutus/', show_about_page),
-    path('home/', show_home_page)
+    path('home/', show_home_page),
+    path('', home),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
